d84bae1f94f9ace7b5f48116ef6efe53b6e86f1f
Anna Cho <loveablessing@gmail.com>
2022-10-12 14:31:39 -0400

    added like button

--- src/Drink.js
+++ src/Drink.js
@@ -5,6 +5,7 @@ function Drink({name, src, id}) {
         <li className="drinks" key={id} style={{width:'150px',height:'150px'}}>
             <div style={{wordWrap: "normal", textAlign: "center"}}>{name}</div>
             <img className="img" src={src}/>
+            <button>❤️</button>
         </li>
     )
 }
