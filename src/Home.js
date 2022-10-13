c3ff2842af4a3be49d4be3f50d0dad06ff76155b
Anna Cho <loveablessing@gmail.com>
2022-10-13 15:49:44 -0400

    changed home page message

--- src/Home.js
+++ src/Home.js
@@ -7,8 +7,7 @@ class Home extends Component {
       <div className="home">
         <h2 style={{fontSize:"40px"}}>Welcome thirsty strangers!</h2>
         <p style={{fontSize:"20px"}}>Care for a drink?</p>
- 
-        <p style={{fontSize:"20px"}}>Discover your next favorite brew here.</p>
+        <p style={{fontSize:"20px"}}>Discover your next favorite brew here or add your favorite to share.</p>
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwfAFHKFkTQwXgIXUnOuKL6zPUMl32XPJphpCCJApLUkjjO-HY75Dmr1y-L6uJ3Qzx16Y&usqp=CAU"/>
       </div>
     );
