524456ce4024295096d697b830683bc829173eb4
Anna Cho <loveablessing@gmail.com>
2022-10-13 15:48:24 -0400

    created margin space between text and image in drink card

--- src/Drink.js
+++ src/Drink.js
@@ -17,7 +17,7 @@ class Drink extends Component{
     render(){
     return (
         <li className="drinks" key={this.props.id} style={{width:'150px',height:'180px'}}>
-            <div style={{wordWrap: "normal", textAlign: "center"}}>{this.props.name}</div>
+            <div style={{wordWrap: "normal", textAlign: "center", marginBottom: "5px"}}>{this.props.name}</div>
             <img className="img" src={this.props.src}/>
             <button onClick={this.incrementMe} style={{marginTop: "5px"}}>❤️Likes: {this.state.count}</button>
         </li>
